69b139aae4771b6c827fda41a50f8a3fd3a086b9
Luca Roverelli <luca.roverelli@gmail.com>
2018-02-15 17:02:41 +0100

    Removed unnecessary imports

--- example/src/index.ts
+++ example/src/index.ts
@@ -2,9 +2,6 @@ import { App, ConfigBuilder } from "lynx-framework";
 
 const port = Number(process.env.PORT) || 3000;
 
-import * as session from "express-session";
-import * as redisStore from "connect-redis";
-
 import LightBootstrapAdminModule from "lynx-light-bootstrap-admin-theme";
 
 let myConfig = new ConfigBuilder(__dirname).build();
